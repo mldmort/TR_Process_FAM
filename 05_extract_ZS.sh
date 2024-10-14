d86dc1bee8656f0a9f563006b84d43cb68204e1d
mldmort <miladmortv@gmail.com>
2024-10-14 12:06:44 -0700

    initial commit

---
+++ 05_extract_ZS.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VCF_IN=../longtr_merge.sorted.vep.addInfo.vcf.gz
+ZS_THR=3
+OUT=output/zs_large_$ZS_THR.txt
+N_SAM=$(bcftools query -l $VCF_IN | wc -l)
+echo "number of samples: $N_SAM"
+
+bcftools query -f '%ID[\t%ZS:%GB]' $VCF_IN | 
+awk -v n_sam=$N_SAM \
+	-v zs_thr=$ZS_THR \
+'BEGIN{FS="\t";OFS="\t"}\
+{\
+	if (NR==FNR) {\
+		sample_dict[NR] = $1;\
+	}\
+	else {\
+		for (col=2;col<=NF;col++) {\
+			split($col, p_col, ":");\
+			zs = p_col[1];\
+			gb = p_col[2];\
+			if (zs != "." && zs != "nan|nan") {\
+				n = split(zs, p_z, "|");\
+				split(gb, p_g, "|");\
+				for (i=1;i<=n;i++) {\
+					abs_zs = p_z[i]; if (abs_zs<0) {abs_zs=-1*abs_zs};\
+					if (abs_zs >= zs_thr) {\
+						print $1, p_z[i], p_g[i], sample_dict[col-1];\
+					}\
+				}\
+			}\
+		}\
+	}\
+}' <(bcftools query -l $VCF_IN) - > $OUT
---
+++ run_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e 
+date
+
+#./00_make_vep_table.sh 2>&1 | tee logs/out_vep.txt
+
+python ./01_make_score_table.py 2>&1 | tee logs/out_score.txt
+
+python ./04_make_combined_table.py 2>&1 | tee logs/out_combine.txt
+
+date
